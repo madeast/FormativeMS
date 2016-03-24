d4ddf66f4203d4b1e901a55a8d4f459f9accd23d
madeast <eeagle25@comcast.net>
2016-03-23 19:08:52 -0600

    Added the School Years as well graphical design

--- src/ms/view/MSPanel.java
+++ src/ms/view/MSPanel.java
@@ -19,22 +19,49 @@ public class MSPanel extends JPanel
 	private JCheckBox twoCheckBox;
 	private JCheckBox threeCheckBox;
 	private JCheckBox fourCheckBox;
-	private JTextField convertTextField;
+	private JTextField majorTextField;
+	private JTextField yearTextField;
 	private JLabel titleLabel;
+	private JLabel yearLabel;
+	private JLabel majorLabel;
+	private JLabel majorTextLabel;
+	private JLabel yearTextLabel;
 	private SpringLayout baseLayout;
 	
 	public MSPanel(MSController baseController)
 	{
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
-		convertTextField = new JTextField("");
+		majorTextField = new JTextField("");
+		yearTextField = new JTextField("");
+		baseLayout.putConstraint(SpringLayout.NORTH, majorTextField, 0, SpringLayout.NORTH, yearTextField);
+		baseLayout.putConstraint(SpringLayout.SOUTH, majorTextField, 0, SpringLayout.SOUTH, yearTextField);
 		titleLabel = new JLabel("This is the college major, and year machine.");
+		yearLabel = new JLabel("School Year");
+		baseLayout.putConstraint(SpringLayout.EAST, yearLabel, -69, SpringLayout.EAST, this);
+		majorLabel = new JLabel("Major");
+		majorTextLabel = new JLabel("Major:");
+		baseLayout.putConstraint(SpringLayout.SOUTH, majorTextLabel, -6, SpringLayout.NORTH, majorTextField);
+		baseLayout.putConstraint(SpringLayout.WEST, majorTextField, 0, SpringLayout.WEST, majorTextLabel);
+		yearTextLabel = new JLabel("School Year:");
+		baseLayout.putConstraint(SpringLayout.NORTH, yearTextLabel, 107, SpringLayout.SOUTH, titleLabel);
+		baseLayout.putConstraint(SpringLayout.SOUTH, yearTextLabel, -150, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, yearTextField, 6, SpringLayout.SOUTH, yearTextLabel);
+		baseLayout.putConstraint(SpringLayout.EAST, yearTextField, -10, SpringLayout.EAST, yearTextLabel);
 		convertButton = new JButton("Decode.");
+		baseLayout.putConstraint(SpringLayout.EAST, majorTextField, -3, SpringLayout.WEST, convertButton);
+		baseLayout.putConstraint(SpringLayout.SOUTH, yearTextField, -60, SpringLayout.NORTH, convertButton);
 		mCheckBox = new JCheckBox("M", false);
+		baseLayout.putConstraint(SpringLayout.WEST, majorLabel, 0, SpringLayout.WEST, mCheckBox);
 		cCheckBox = new JCheckBox("CS", false);
+		baseLayout.putConstraint(SpringLayout.WEST, majorTextLabel, 46, SpringLayout.EAST, cCheckBox);
+		baseLayout.putConstraint(SpringLayout.WEST, yearTextField, 174, SpringLayout.EAST, cCheckBox);
+		baseLayout.putConstraint(SpringLayout.SOUTH, majorLabel, -13, SpringLayout.NORTH, cCheckBox);
 		iCheckBox = new JCheckBox("IT", false);
 		oneCheckBox = new JCheckBox("1st", false);
+		baseLayout.putConstraint(SpringLayout.SOUTH, yearLabel, -6, SpringLayout.NORTH, oneCheckBox);
 		twoCheckBox = new JCheckBox("2nd", false);
+		baseLayout.putConstraint(SpringLayout.EAST, yearTextLabel, -15, SpringLayout.WEST, twoCheckBox);
 		threeCheckBox = new JCheckBox("3rd", false);
 		fourCheckBox = new JCheckBox("4th", false);
 		
@@ -47,8 +74,13 @@ public class MSPanel extends JPanel
 	{
 		this.setLayout(baseLayout);
 		this.setBackground(Color.GRAY);
-		this.add(convertTextField);
+		this.add(majorTextField);
+		this.add(yearTextField);
 		this.add(titleLabel);
+		this.add(majorLabel);
+		this.add(yearLabel);
+		this.add(majorTextLabel);
+		this.add(yearTextLabel);
 		this.add(convertButton);
 		this.add(mCheckBox);
 		this.add(cCheckBox);
@@ -65,7 +97,6 @@ public class MSPanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.SOUTH, convertButton, -10, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, titleLabel, 10, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, titleLabel, 117, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, convertTextField, 53, SpringLayout.SOUTH, titleLabel);
 		baseLayout.putConstraint(SpringLayout.NORTH, iCheckBox, 0, SpringLayout.NORTH, fourCheckBox);
 		baseLayout.putConstraint(SpringLayout.NORTH, fourCheckBox, 196, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, fourCheckBox, 328, SpringLayout.WEST, this);
@@ -80,9 +111,6 @@ public class MSPanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.WEST, cCheckBox, 0, SpringLayout.WEST, mCheckBox);
 		baseLayout.putConstraint(SpringLayout.SOUTH, cCheckBox, -6, SpringLayout.NORTH, mCheckBox);
 		baseLayout.putConstraint(SpringLayout.WEST, mCheckBox, 50, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, convertTextField, 0, SpringLayout.WEST, convertButton);
-		baseLayout.putConstraint(SpringLayout.SOUTH, convertTextField, -142, SpringLayout.NORTH, convertButton);
-		baseLayout.putConstraint(SpringLayout.EAST, convertTextField, 73, SpringLayout.WEST, convertButton);
 		
 	}
 	
@@ -92,7 +120,55 @@ public class MSPanel extends JPanel
 		{
 			public void actionPerformed(ActionEvent click)
 			{
-				convertTextField.setText("Mathematics");
+				majorTextField.setText("Mathematics");
+			}
+		});
+		
+		cCheckBox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				majorTextField.setText("Computer Science");
+			}
+		});
+		
+		iCheckBox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				majorTextField.setText("Information Technology");
+			}
+		});
+		
+		oneCheckBox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				yearTextField.setText("Freshman");
+			}
+		});
+		
+		twoCheckBox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				yearTextField.setText("Sophmore");
+			}
+		});
+		
+		threeCheckBox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				yearTextField.setText("Junior");
+			}
+		});
+		
+		fourCheckBox.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				yearTextField.setText("Senior");
 			}
 		});
 	}
